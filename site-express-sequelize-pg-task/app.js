ca047a0fd128d8e126c5adf3737cafe9e7c5e964
drawlabs <ryan@drawlabs.com>
2014-04-27 21:57:16 -0700

    Update packages and express 4 usage.

--- site-express-sequelize-pg-task/app.js
+++ site-express-sequelize-pg-task/app.js
@@ -1,28 +1,37 @@
-var express = require('express')
-  , routes  = require('./routes')
-  , user    = require('./routes/user')
-  , task    = require('./routes/task')
-  , http    = require('http')
-  , path    = require('path')
-  , db      = require('./models')
- 
-var app = express()
- 
-// all environments
+var http           = require('http');
+var path           = require('path');
+var express        = require('express');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+var routes         = require('./routes');
+var user           = require('./routes/user');
+var task           = require('./routes/task');
+var db             = require('./models');
+
+
+var app            = express();
+
 app.set('port', process.env.PORT || 3000)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
-app.use(express.favicon())
-app.use(express.logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.methodOverride())
-app.use(app.router)
-app.use(express.static(path.join(__dirname, 'public')))
- 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
+
+app.use(express.static(__dirname + '/public'));   // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));           // log every request to the console
+app.use(bodyParser());            // pull information from html in POST
+app.use(methodOverride());          // simulate DELETE and PUT
+
+//app.use(express.favicon())
+//app.use(express.logger('dev'))
+//app.use(express.json())
+//app.use(express.urlencoded())
+//app.use(express.methodOverride())
+//app.use(app.router)
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+   // configure stuff here
 }
  
 app.get('/', routes.index)
